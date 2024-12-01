49aaa8ba651b21877c086523a2f09c8d6ebda9c7
shahrazmirza <shahrazahmedmirza@live.com>
2024-12-02 00:36:16 +1100

    Commit

--- src/app/components/Models/Monkey.js
+++ src/app/components/Models/Monkey.js
@@ -1,35 +1,18 @@
 "use client";
-import {
-  ContactShadows,
-  Environment,
-  OrbitControls,
-  PerspectiveCamera,
-  useGLTF,
-} from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import React from "react";
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
 
-export default function Monkey() {
-  const gltf = useGLTF("/monkey.glb");
-
+export function Monkey(props) {
+  const { nodes, materials } = useGLTF("/monkey.glb");
   return (
-    <Canvas
-      style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-      }}
-    >
-      <color attach="background" args={["#eee540"]} />
-      <Environment preset="studio" />
-      <PerspectiveCamera makeDefault position={[0, 0, 5]} />
-      <OrbitControls />
-      <ContactShadows />
-
-      <primitive object={gltf.scene} position={[0, 0, 0]} />
-    </Canvas>
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Suzanne.geometry}
+        material={nodes.Suzanne.material}
+      />
+    </group>
   );
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,44 @@
+"use client";
+import {
+  ContactShadows,
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 import React from "react";
-import Monkey from "./components/Models/Monkey";
+import { Monkey } from "./components/Models/Monkey";
+import { Slippers } from "./components/Models/Slippers";
 
-function Home() {
+export default function Home() {
   return (
-    <div>
-      <Monkey />
+    <div className="flex h-screen">
+      <Canvas
+        style={{
+          width: "50%",
+          height: "100%",
+        }}
+      >
+        <color attach="background" args={["#eee540"]} />
+        <Environment preset="studio" />
+        <PerspectiveCamera makeDefault position={[2, 3.9, 4.1]} />
+        <OrbitControls />
+        <Monkey position={[0, 0.1, 0]} />
+        <ContactShadows />
+      </Canvas>
+      {/* <Canvas
+        style={{
+          width: "50%",
+          height: "100%",
+        }}
+      >
+        <color attach="background" args={["#eee"]} />
+        <Environment preset="studio" />
+        <PerspectiveCamera makeDefault position={[2, 3.9, 4.1]} />
+        <OrbitControls />
+        <Slippers position={[0, 0.1, 0]} />
+        <ContactShadows />
+      </Canvas> */}
     </div>
   );
 }
-
-export default Home;
