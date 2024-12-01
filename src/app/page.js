565978c44656c09a2eea1ea607a365346f3f6438
shahrazmirza <shahrazahmedmirza@live.com>
2024-12-02 00:40:11 +1100

    Commit

--- src/app/page.js
+++ src/app/page.js
@@ -8,37 +8,26 @@ import {
 import { Canvas } from "@react-three/fiber";
 import React from "react";
 import { Monkey } from "./components/Models/Monkey";
-import { Slippers } from "./components/Models/Slippers";
 
 export default function Home() {
   return (
     <div className="flex h-screen">
       <Canvas
         style={{
-          width: "50%",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
           height: "100%",
         }}
       >
         <color attach="background" args={["#eee540"]} />
         <Environment preset="studio" />
-        <PerspectiveCamera makeDefault position={[2, 3.9, 4.1]} />
+        <PerspectiveCamera makeDefault position={[0, 0, 5]} />
         <OrbitControls />
         <Monkey position={[0, 0.1, 0]} />
         <ContactShadows />
       </Canvas>
-      {/* <Canvas
-        style={{
-          width: "50%",
-          height: "100%",
-        }}
-      >
-        <color attach="background" args={["#eee"]} />
-        <Environment preset="studio" />
-        <PerspectiveCamera makeDefault position={[2, 3.9, 4.1]} />
-        <OrbitControls />
-        <Slippers position={[0, 0.1, 0]} />
-        <ContactShadows />
-      </Canvas> */}
     </div>
   );
 }
