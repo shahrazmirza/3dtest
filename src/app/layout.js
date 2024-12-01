e3b5320286020373ff45768930723749aa7d5c0d
shahrazmirza <shahrazahmedmirza@live.com>
2024-12-02 00:26:26 +1100

    Commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,10 @@
 import "./globals.css";
 
+export const metadata = {
+  title: "NSW Central West Muslim Association (NSWCWMA)",
+  description: "Public Benevolent Institute",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
